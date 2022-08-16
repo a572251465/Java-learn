04d29436c28451384ac838c647698a218b5beec3
lihaohao <2495041749@qq.com>
2022-08-16 12:21:35 +0800

    feat: hello world java

---
+++ Java-basics/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @Author lihh
+ * @Date 2022/8/16 - 08 - 16
+ * @Description: Java-learn
+ * @version: 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world java");
+    }
+}
